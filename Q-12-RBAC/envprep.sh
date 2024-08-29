aee29c7acc3e4a23c03ccc10852d9e6f3881e166
vagrant <vagrant@hellocloud-native-box>
2024-08-29 07:11:13 +0000

    update Q-12

--- Q-12-RBAC/envprep.sh
+++ Q-12-RBAC/envprep.sh
@@ -6,5 +6,5 @@ kubectl create sa service-account-web -n db
 kubectl create role pods-get --verb=create,delete,get --resource=deployments,pods -n db
 kubectl create rolebinding pod-get-binding --role=pods-get --serviceaccount=db:service-account-web -n db
 
-sleep 5
-kubectl run pod web-pod --image=nginx --dry-run=client -n db -o yaml > web-pod.yaml
+sleep 1
+kubectl apply-f dev-pod.yaml
